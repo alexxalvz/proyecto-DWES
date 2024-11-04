0e71e257570d693f3f120011fe12cbce347e3b5b
alexxalvz <riesgo.alex29@gmail.com>
2024-11-04 20:46:42 +0100

    En este commit creo la clase Profesor extendida de Miembro

---
+++ clases/Profesor.php
@@ -0,0 +1,25 @@
+<?php
+//Aqui incluyo la clase Miembro por si me da algun error de union entre clases
+include_once 'Miembro.php';
+class Profesor extends Miembro {
+    //Atributos
+    private int $numeroAsignaturas;
+    private boolean $titular;
+
+    //Constructor
+    public function __construct(int $id, string $nombre, string $apellidos, string $email, int $numeroAsignaturas) {
+        parent::__construct($id, $nombre, $apellidos, $email);
+        $this->numeroAsignaturas = $numeroAsignaturas;
+    }
+
+    //Funcion para devolver un array de datos de profesores.
+    public static function crearProfesoresMuestra(): array {
+        return [
+            new Profesor(1, "Steve", "Wozniak", "steve@apple.com", 3),
+            new Profesor(2, "Ada", "Lovelace", "ada@gmail.com", 2),
+            new Profesor(3, "Taylor", "Otwell", "taylor@laravel.com", 3 ),
+            new Profesor(4, "Rasmus", "Lerdoff", "rasmus@php.com", 5)
+        ];
+    }
+}
+?>
