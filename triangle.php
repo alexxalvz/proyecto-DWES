534d3aa16a1ceb91cbe86897fd914f2d55794c5b
alexxalvz <riesgo.alex29@gmail.com>
2024-11-04 21:02:52 +0100

    commit para generar el index del triangulo y mostrarlo por pantalla

---
+++ triangle.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
+<style>
+    body {
+    font-family: monospace;
+    }
+</style>
+</head>
+<body>
+<?php
+//Para llamar a la funcion del Generador del triangulo y mostrar por pantalla.
+require ("./clases/TriangleGenerator.php");
+echo TriangleGenerator::generateTriangle(6);
+?>
+</body>
+</html>
